c0aa49de9cf9b178c75db976c6c53332a19a6207
Lmpwybb <cartoux.sebastien@orange.fr>
2020-03-26 16:08:48 +0100

    Ajout de npm et webpack encore

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
