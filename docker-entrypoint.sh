c72891a26173f92680bc7c49fb89e7578a8c992e
Sébastien Cartoux <cartoux.sebastien@orange.fr>
2022-06-28 19:30:44 +0200

    [FIX] Fixed docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd /var/www
 composer install
-npm install --force && npm run-script build
+npm install && npm run dev
 /usr/bin/env php /var/www/bin/console doctrine:schema:update --force && \
 /usr/bin/env php /var/www/bin/console doctrine:fixtures:load --append
 exec docker-php-entrypoint "$@"
